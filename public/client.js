90a6440730fb95303fb301230f84b6bb819bc1cf
alehtosalo <alehtosalo@gmail.com>
2018-08-14 18:19:48 +0300

    testing

---
+++ public/client.js
@@ -0,0 +1,7 @@
+var userName = prompt("Anna kayttajanimi","");
+if (userName != null) {
+    document.getElementById("username"),value = userName;}
+
+var userPswd = prompt("Anna salasana","");
+if (userPswd != null) {
+    document.getElementById("password").value = userPswd;}
